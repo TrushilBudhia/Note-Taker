bbfeb788f67487c72ef3c2de306f7af72e5c2509
Trushil Budhia <trushil.budhia@gmail.com>
2021-05-19 12:18:48 +0800

    Updated server.js and package,json and renamed noteData.js file to note-data.js

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ let database = require('./db/db.json');
 
 const PORT = process.env.PORT || 3000;
 
-
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
